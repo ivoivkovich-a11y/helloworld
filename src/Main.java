1fc8c4d3be9e17d33229f484afbed77e5597f928
ivoivkovich <ivoivkovich@gmail.com>
2026-03-13 16:32:53 +0300

    остановился на 3 задании

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,36 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир");
-        System.out.println("Hello world");
+// Задание №1
+        int syringes = 1303445433;
+        byte treatmentDepartment = 85;
+        short nurses = 30234;
+        long injections = 7344344678845435432L;
+        float workLoadNurses = 32.3f;
+        double sizeBacteria = 23.2333233456;
+            System.out.println("Значение переменной syringes с типом int равно " + syringes); //Строка №1
+            System.out.println("Значение переменной " + "treatmentDepartment" + " с типом" + " byte" + " равно " + treatmentDepartment); //Строка №2
+            System.out.println("Значение переменной syringes с типом short равно " + nurses);
+            System.out.println("Значение переменной syringes с типом long равно " + injections);
+            System.out.println("Значение переменной syringes с типом float равно " + workLoadNurses);
+            System.out.println("Значение переменной syringes с типом double равно " + sizeBacteria);
+//Комментарий к заданию №1. Тест можно написать минимум двумя вариантами как в строках №1 и №2. Я считаю , что более читабильно было как в строке №1 (весь текст удобнее читается)
+// Задание №2
+        float a = 27.12f;
+        long b = 987678965549L;
+        float c = 2.786f;
+        int d = 569;
+        int i = -159;
+        int f = 27897;
+        byte g = 67;
+// Задание №3
+        byte studentLudmilaPavlovna = 23;
+        byte studentAnnaSergeevna = 27;
+        byte studentEkaterinaAndreevna = 30;
+        int totalPaper = 480;
+        int paperStudent = totalPaper / (studentLudmilaPavlovna + studentAnnaSergeevna + studentEkaterinaAndreevna);
+        System.out.println(paperStudent);
+
+
     }
 }
